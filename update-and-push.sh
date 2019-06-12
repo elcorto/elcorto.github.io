8b7178381032adf99cc0e78393f28152b9318131
Steve Schmerler <git@elcorto.com>
2019-06-13 01:31:37 +0200

    API: update-and-push.sh: require project arg

--- update-and-push.sh
+++ update-and-push.sh
@@ -1,7 +1,8 @@
 #/bin/sh
 
-mkdir -p pwtools
-rsync -auvz --del ../pwtools/doc/build/html/* pwtools/
-git add pwtools
-git ci -m "doc for pwtools commit $(git -C ../pwtools log -1 --format='%H')"
+project=$1
+mkdir -p $project
+rsync -auvz --del ../$project/doc/build/html/* $project/
+git add $project
+git ci -m "doc for $project commit $(git -C ../$project log -1 --format='%H')"
 git push origin master
